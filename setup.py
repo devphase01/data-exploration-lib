4fdd8daf4bfd1553374f5c81e5ca58a7c83270b4
Ihor Voznyi <ihor.voznyi@tech-stack.io>
2023-11-02 23:14:23 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DataExplorer',
+    version='0.1',
+    packages=find_packages(),
+    description='A library for data exploration',
+    author='Ihor Voznyi, *author*, *author*',
+    author_email='ihor.voznyi.sa.2021@lpnu.ua, *email*, *email*',
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'seaborn',
+    ],
+)
