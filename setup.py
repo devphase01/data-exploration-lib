f9c8e2dc435547ba5345e23d167d4cf1056fb496
somykOS <philipin1337@gmail.com>
2023-12-14 16:56:31 +0200

    +normalization+dist-analysis

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DataExplorer',
-    version='0.1',
+    name="DataExplorer",
+    version="0.1",
     packages=find_packages(),
-    description='A library for data exploration',
-    author='Ihor Voznyi, *author*, *author*',
-    author_email='ihor.voznyi.sa.2021@lpnu.ua, *email*, *email*',
+    description="A library for data exploration",
+    author="Ihor Voznyi, *author*, *author*",
+    author_email="ihor.voznyi.sa.2021@lpnu.ua, *email*, *email*",
     install_requires=[
-        'pandas',
-        'matplotlib',
-        'seaborn',
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scikit-learn",
     ],
 )
